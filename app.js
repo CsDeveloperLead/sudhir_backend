2ecf74aeebade1f4f2c39136e7982b0fb1e63134
Aftab1311 <md.aftabehsan@gmail.com>
2024-12-24 12:04:36 +0530

    comment try 1

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(
 );
 app.use(express.json({ limit: '16kb' }))
 app.use(express.urlencoded({ extended: true, limit: '16kb' }))
-app.use(express.static('public'))
+app.use(express.static('tmp'))
 
 
 //import router here
