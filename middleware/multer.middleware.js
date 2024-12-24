7c594177130ed353b81098cfb44b04a0dab77629
Aftab1311 <md.aftabehsan@gmail.com>
2024-12-24 12:28:56 +0530

    comment try 1

--- middleware/multer.middleware.js
+++ middleware/multer.middleware.js
@@ -1,16 +1,21 @@
-import multer from 'multer'
+import multer from 'multer';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const DIR = "./tmp"
+// Get the current directory path using import.meta.url
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, DIR)
+        // Use __dirname to construct the path to the 'temp' folder
+        cb(null, path.join(__dirname, 'public', 'temp'));
     },
     filename: (req, file, cb) => {
-        cb(null, file.originalname)
+        cb(null, file.originalname);
     }
-})
+});
 
 export const upload = multer({
     storage
-})
+});
